39dbb4a15fb69ffd8af74237d9f2069a21634f1b
Georgina Davy <a1829307@student.adelaide.edu.au>
2022-03-18 15:34:59 +1030

    2-2 fixed

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -2,13 +2,16 @@
 
 int binary_to_number(int binary_digits[], int number_of_digits){
     int sum = 0;
+    int f = number_of_digits - 1;
     for(int i = 0; i < number_of_digits; i++){
         if(binary_digits[i] == 0){
-            int x = pow(2,i);
+            int x = pow(2,f);
             sum = sum + x;
+            f--;
         }else{
-            int x = pow(2,i) + 1;
+            int x = pow(2,f) + 1;
             sum = sum + x;
+            f--;
         }
     }
     return sum;
